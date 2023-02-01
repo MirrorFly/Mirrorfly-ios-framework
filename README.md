14cffe173a22d5ff9ad76e9be6a43dc69ea17daa
Hariram <hariram.c@contus.in>
2023-02-01 19:15:10 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mirrorfly-ios-framework
-Make an easy and efficient way with CONTUS MirrorFly Chat SDK for Flutter - simply integrate the real-time chat features and functionalities into a client's app.
+
+A description of this package.
---
+++ Tests/Mirrorfly-ios-frameworkTests/Mirrorfly_ios_frameworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Mirrorfly_ios_framework
+
+final class Mirrorfly_ios_frameworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Mirrorfly_ios_framework().text, "Hello, World!")
+    }
+}
