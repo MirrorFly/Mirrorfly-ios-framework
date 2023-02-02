b8d3256cf5f37b5b88ccc9b007c8a51cb0f62a8c
Hariram <hariram.c@contus.in>
2023-02-02 16:58:55 +0530

    added flt database

--- Package.swift
+++ Package.swift
@@ -13,11 +13,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1")),
+        .package(url: "https://github.com/iziz/libPhoneNumber-iOS", .upToNextMajor(from: "1.0.4")),
+        .package(url: "https://github.com/realm/realm-swift", .upToNextMajor(from: "10.34.1"))
+       
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "Mirrorfly-ios-framework", path: "./Sources/Mirrorfly-ios-framework.xcframework")
+        .binaryTarget(name: "Mirrorfly-ios-framework", path: "./Sources/Test/Mirrorfly-ios-framework.xcframework"),
+        .binaryTarget(name: "FlyDatabase", path: "./Sources/Test/FlyDatabase.xcframework")
+        
     ]
 )
