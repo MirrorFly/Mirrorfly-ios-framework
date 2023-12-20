a1fda9e3a9cbd1d74bf4b6385e72dcd5047b979c
Amose Vasanth S <amosevasanth.s@contus.in>
2023-12-20 18:38:46 +0530

    #package changes

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MirrorFlySDK_Github",
-            path: "SDK"),
+            dependencies: []),
         .testTarget(
             name: "MirrorFlySDK_GithubTests",
             dependencies: ["MirrorFlySDK_Github"]),
