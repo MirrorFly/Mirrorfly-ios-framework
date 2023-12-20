e07a9bcc323ef8410cdec6a0d4bdaaaba7291af1
Amose Vasanth S <amosevasanth.s@contus.in>
2023-12-20 18:24:14 +0530

    CONTUIMSER-37606-SwiftPackage - changes

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MirrorFlySDK_Github",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MirrorFlySDK_Github",
+            targets: ["MirrorFlySDK_Github"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MirrorFlySDK_Github"),
+        .testTarget(
+            name: "MirrorFlySDK_GithubTests",
+            dependencies: ["MirrorFlySDK_Github"]),
+    ]
+)
