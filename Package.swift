d0f8d1a6a3ad2296df48e4bcce3d87969f93abb3
amoseContus <134388905+amoseContus@users.noreply.github.com>
2023-12-20 17:13:22 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MirrorFlySDK",
-            targets: ["MirrorFlySDK"]),
+            name: "MirrorFlySDK_Github",
+            targets: ["MirrorFlySDK_Github"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MirrorFlySDK"),
+            name: "MirrorFlySDK_Github"),
         .testTarget(
-            name: "MirrorFlySDKTests",
-            dependencies: ["MirrorFlySDK"]),
+            name: "MirrorFlySDK_GithubTests",
+            dependencies: ["MirrorFlySDK_Github"]),
     ]
 )
