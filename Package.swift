5858cb66f35d1aa07af6a619dd0983ac34e62f48
Amose Vasanth S <amosevasanth.s@contus.in>
2023-12-21 17:07:54 +0530

    #package change

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MirrorFlySDK_Github",
-            dependencies: ["Alamofire"]),
+            dependencies: [.product(name: "Alamofire", package: "Alamofire")]),
         .binaryTarget(name: "MirrorFlySDK",
                       path: "SDK/MirrorFlySDK.xcframework"),
         .testTarget(
