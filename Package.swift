dccd00bc983447512e2c578f21ed61c8c016786a
Amose Vasanth S <amosevasanth.s@contus.in>
2023-12-21 10:17:24 +0530

    #package change

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "MirrorFlySDK_Github",
+    name: "MirrorFlySDK",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MirrorFlySDK_Github",
+            name: "MirrorFlySDK",
             targets: ["MirrorFlySDK"]),
     ],
 //    targets: [
