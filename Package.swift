bb37a6b816004bc1877b2f9c67728d5a0c26c5dd
Amose Vasanth S <amosevasanth.s@contus.in>
2023-12-21 12:04:37 +0530

    #package change

--- Package.swift
+++ Package.swift
@@ -8,20 +8,20 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MirrorFlySDK_Github",
-            targets: ["MirrorFlySDK_Github","MirrorFlySDK"]),
+            name: "MirrorFlySDK",
+            targets: ["MirrorFlySDK"]),
     ],
 
     targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
             .target(
-                name: "MirrorFlySDK_Github",
+                name: "MirrorFlySDK",
                 dependencies: []),
             .binaryTarget(name: "MirrorFlySDK",
                           path: "SDK/MirrorFlySDK.xcframework"),
             .testTarget(
-                name: "MirrorFlySDK_GithubTests",
-                dependencies: ["MirrorFlySDK_Github"]),
+                name: "MirrorFlySDKTests",
+                dependencies: ["MirrorFlySDK"]),
         ]
 )
